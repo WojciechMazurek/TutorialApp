326fecb9941f035199e1b2f776cc0736856404b8
Wojciech Mazurek <31115423+WojciechMazurek@users.noreply.github.com>
2023-02-14 16:13:15 -0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductModel } from './card/product.model';
+import { mock_list } from './card/mock_list';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TutorialApp';
+  products: ProductModel [] = [];
+
+  constructor(){
+    for (var item of mock_list){
+      console.log(item);
+      this.products.push(item);
+    }
+  }
 }
